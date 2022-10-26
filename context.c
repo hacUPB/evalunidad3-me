a9221ba95c79fe32d3856bb10467911d159845d7
Nivekke <110250498+Nivekke@users.noreply.github.com>
2022-10-25 20:06:12 -0500

    Update context.c

--- context.c
+++ context.c
@@ -4,24 +4,30 @@
 #include "context.h"
 
 
-void __DoSomeBusinessLogic(void *this){
-
-    Context *context = (Context *)malloc(sizeof(Context));
-
-    strategy_t* strategy = (strategy_t *)this;
-    printf("%s = a, b, c, d, e\n", strategy->list)
-        
-    strategy_t *strategy_new(){
-        return (strategy_t *)malloc(sizeof(strategy_t));
-    }
+IStrategy *strategy_new()
+{
+    return (IStrategy *)malloc(sizeof(IStrategy));
 }
 
-void strategy_ctor(strategy_t *this){
-    this->list = (char*)malloc(6*sizeof(char));
-    strcpy(this->list, "a", "b", "c", "d", "e");
-    this->strategy_func = &__DoSomeBusinessLogic;
+void __SetStrategy(void *this){
+    IStrategy *_strategy = (IStrategy *)this;
 }
 
-void strategy_dtor(strategy_t*this){
-    free(this->list);
+void __DoSomeBusinessLogic(IStrategy* this, char data){
+    
+    IStrategy *_strategy = (IStrategy *)this;
+    this->_strategy.DoAlgorithm = data;
+    printf("a", "b", "c", "d", "e", _strategy->data);
+
+}
+
+void context_ctor(IStrategy *this, char data){
+    IStrategy *_strategy = (IStrategy *)this;
+    this->data = (char *)malloc((strlen(data) + 1) * sizeof(char));
+    strcpy(this->data, data);
+    this->_strategy = _strategy;
+}
+
+void context_dtor(IStrategy *this){
+    free(this)
 }
